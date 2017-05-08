9feeddea8f7d7f985cb8186102e3e7d2ea983bc3
Kay Krewson <kay.k@comcast.net>
2017-05-08 09:34:32 -0700

    basic books, authors .js files in place

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var knex = require('../db/connection')
 
-/* GET users listing. */
+// ===== GET ALL BOOKS =====
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource')
 });
 
-module.exports = router;
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var knex = require('../db/connection')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' })
 });
 
-module.exports = router;
+module.exports = router
